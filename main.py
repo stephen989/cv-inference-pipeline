86f67a23b8ce76f6daf9e677043b106967a7e8d4
Stephen O'Brien <Stephen.OBrien1@ie.ey.com>
2022-10-20 10:12:30 +0100

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,71 @@
-from test_pipeline import *
 import argparse
+from model_setup import *
+from tqdm import tqdm
 
 os.chdir("..")
+
+
+def image_pipeline(opts):
+
+    image_dir = opts.image_dir
+    image_ext = opts.image_ext
+    image_output_dir = opts.image_output_dir
+    output_yaml = opts.yaml_output
+    weights = opts.weights
+    model_classes_file = opts.model_classes
+
+    os.makedirs(image_output_dir, exist_ok=True)
+    frames, outputs, image_names = image_preprocessing(image_dir, image_ext)
+    outputs_dict = {"Directory": image_dir,
+                    "Extension": image_ext,
+                    "Output directory": image_output_dir,
+                    "Preprocessing output": outputs,
+                    "Model Outputs": dict()}
+
+    model = Model(weights, opts.model_version, model_classes_file)
+    outputs_dict["Model classes"] = model.classes
+    print("Feeding model")
+    for image_name, frame in tqdm(zip(image_names, frames)):
+        output = model(frame)
+        outputs_dict["Model Outputs"][image_name] = output
+    print("Complete")
+    # write to yaml file
+    with open(output_yaml, 'w') as y:
+        yaml.dump(outputs_dict, y)
+    print(f"Saved run output to {output_yaml}")
+    create_output_images(output_yaml, frames,  image_output_dir)
+    print(f"Saved output images to {image_output_dir}")
+    return True
+
+
+def video_pipeline(opts):
+    video = opts.video_input
+    output_video = opts.video_output
+    output_yaml = opts.yaml_output
+    weights = opts.weights
+    model_classes = opts.model_classes
+    frames, outputs = video_prepocessing(video)
+    outputs_dict = {"File": video,
+                    "Output Video": output_video,
+                    "Preprocessing output": outputs,
+                    "Model Outputs": dict()}
+
+    model = Model(weights, opts.model_version, model_classes)
+    outputs_dict["Model classes"] = model.classes
+
+    # feed through model one by one
+    print("Feeding model")
+    for i, frame in enumerate(tqdm(frames, unit="frame")):
+        output = model(frame)
+        outputs_dict["Model Outputs"][i] = output
+    print("Complete")
+    # write to yaml file
+    with open(output_yaml, 'w') as y:
+        yaml.dump(outputs_dict, y)
+    print(f"Saved run output to {output_yaml}")
+    create_output_video(output_yaml, frames, output_video, video)
+    print(f"Saved output video to {output_video}")
+    return True
 
 
 def main(opts):
@@ -31,10 +95,9 @@ def parse_opt(known=False):
     parser.add_argument('--image_dir', type=str, default='minidata/test/images')
     parser.add_argument('--image_ext', type=str, default='.jpg')
     parser.add_argument('--image_output_dir', type=str, default='output_imagesbad')
-    parser.add_argument('--video_input', type=str, default='')
+    parser.add_argument('--video_input', type=str, default='inference_pipeline/data/test.mp4')
     parser.add_argument('--video_output', type=str, default='oil_output.mp4')
     parser.add_argument('--yaml_output', type=str, default='test_pipeline.yaml')
-    parser.add_argument('--model', type=str, default='yolo')
     parser.add_argument('--model_version', type=str, default='yolov5s')
     parser.add_argument('--weights', type=str, default='default')
     parser.add_argument('--model_classes', type=str, default='')
@@ -44,5 +107,5 @@ def parse_opt(known=False):
 
 
 if __name__ == "__main__":
-    opts = parse_opt()
-    main(opts)
+    options = parse_opt()
+    main(options)
